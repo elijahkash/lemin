c915be12256ca10b03fa3aa11ef1f70c7b3b2b69
Midas Trisha <mtrisha@student.21-school.ru>
2019-12-05 16:40:49 +0300

    in work

--- includes/farm.h
+++ includes/farm.h
@@ -6,7 +6,7 @@
 /*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 20:34:05 by mtrisha           #+#    #+#             */
-/*   Updated: 2019/12/05 15:11:27 by mtrisha          ###   ########.fr       */
+/*   Updated: 2019/12/05 16:40:43 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 
 # include <libft.h>
 
-typedef __uint32_t	uint;
+typedef __uint32_t	t_uint;
 
 /*
-** From here on will be assume that in our ant's farm can't consist more than
+** From here on will be assumed that our ant's farm can't consist from more than
 ** (__UINT32_MAX__ >> 2) rooms (can be written in a 30-bits number).
-** Otherwise, in case (__UINT32_MAX__ >> 2) + 1 rooms, at least 20 GB of RAM
+** Otherwise, in case ((__UINT32_MAX__ >> 2) + 1) rooms, at least 20 GB of RAM
 ** will be required for this programm (and this is in the case of zero
 ** connections).
 ** This trick will allow us to store each graph node in 8 bytes, and each
 ** connection only in 4 bytes (against 12 and 8 bytes respectively).
 ** This, in turn, will significantly increase efficience of this programm.
 ** Therefore, if in the process of reading, more then (__UINT32_MAX__ >> 2)
-** rooms will be found, (even in case of sufficiency RAM),
+** rooms will be found, (even in case of sufficiency of RAM),
 ** input ERROR will be generated!
 */
 # define MAX_NODES (__UINT32_MAX__ >> 2)
@@ -38,24 +38,24 @@ typedef __uint32_t	uint;
 */
 typedef struct		s_node
 {
-	uint			count_connects : 30;
-	uint			marked : 1;
-	uint			separate : 1;
-	uint			marked_in : 1;
-	uint			marked_out : 1;
-	uint			bfs_level : 30;
+	t_uint			count_connects : 30;
+	t_uint			marked : 1;
+	t_uint			separate : 1;
+	t_uint			marked_in : 1;
+	t_uint			marked_out : 1;
+	t_uint			bfs_level : 30;
 }					t_node;
 
 typedef struct		s_connect
 {
-	uint			dst : 30;
-	uint			state : 2;
+	t_uint			dst : 30;
+	t_uint			state : 2;
 }					t_connect;
 
 /*
 ** Since we can't delete connections (thay can be reversed more then 1 times),
 ** we mark "deleted" ways as forbidden, and, if we pass through negative
-** directions, both directions (and negative, and forbidden) turning into
+** directions, both directions (and negative, and forbidden) turn into
 ** normal ways (marked as BASE_STATE).
 */
 # define CONNECT_BASE_STATE	0
@@ -63,21 +63,41 @@ typedef struct		s_connect
 # define CONNECT_FORBIDDEN	2
 
 /*
+** 1) size = t_farm.names.curlen !
+** 2) here is struct of memory in *mem:
+** [node_0 (count_con = i)][con_1]...[con_i],
+** [node_1 (count_con = j)][con_1]...[con_j],
+** ...
+** [node_(size-1) (count_con = k)][con_1]...[con_k].
+** 3) **nodes pointed to <node_0, node_1 ... node_(size-1)> in *mem.
 **
+** Some notes about graph:
+** We are expecting good input data and we are not deleting any nodes
+** and connections, even if they don't have any profit for algorithm.
+** This means that we will place even those nodes in the graph,
+** that do not have any connections or belong to a connection component,
+** other than the one, in which the start/end node is located.
+** Also, it means, that we will place in the graph dead ends, and
+** nodes, unreacheble from start (before end), but reacheble from end.
+** Also, it means, that during the operation of the algorithm,
+** no connection or nodes will be lost.
+**
+** Some
 */
 typedef struct		s_graph
 {
+	void			**nodes;
 	void			*mem;
 	size_t			size;
-	uint			start;
-	uint			end;
+	t_uint			start;
+	t_uint			end;
 }					t_graph;
 
 typedef struct		s_graph_iterator
 {
-	uint			i;
-	uint			node;
-	uint			type;
+	t_uint			i;
+	t_uint			node;
+	t_uint			type;
 }					t_iter;
 
 # define ITER_ALL				1
@@ -102,16 +122,16 @@ typedef struct		s_full_connect
 
 typedef struct		s_way
 {
-	uint			*nodes;
-	uint			len;
-	uint			border; // no need!!!!!!!
+	t_uint			*nodes;
+	t_uint			len;
+	t_uint			border; // no need!!!!!!!
 	long long		ants;
 }					t_way;
 
 typedef struct		s_enum_ways
 {
 	t_way			*ways;
-	uint			count;
+	t_uint			count;
 }					t_enum_ways;
 
 #endif
